0a1dbe43c3cd4132349da84e7a62e4d64b693027
Angel-Karasu <angel.karasu@proton.me>
2024-04-13 19:08:17 +0200

    Change search product url

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_sortby_methods() -> list[str]: return [sortby.name for sortby in SortBy]
 @app.get('/get_supermarkets')
 def get_supermarkets() -> list[SuperMarket]: return SUPERMARKETS.supermarkets
 
-@app.get('/search_product/{search}')
+@app.get('/search_product')
 def search_product(
     search:str, page:int = 1, sortby:SortBy = SortBy.relevant, descending_order:bool = False,
     list_supermarket_id:list[int] = Query([0])
@@ -27,7 +27,7 @@ def search_product(
 @app.get('/supermarket/{id}/get_supermarket')
 def supermarket_get_supermarket(id:int = Path(ge=0, lt=SUPERMARKETS.max_id)) -> SuperMarket: return SUPERMARKETS.supermarkets[id]
 
-@app.get('/supermarket/{id}/search_product/{search}')
+@app.get('/supermarket/{id}/search_product')
 def supermarket_search_product(
     search:str, page:int = 1, sortby:SortBy = SortBy.relevant, descending_order:bool = False,
     id:int = Path(ge=0, lt=SUPERMARKETS.max_id)    
