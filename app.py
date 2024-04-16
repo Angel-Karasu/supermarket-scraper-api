a676a44aaa9f1d7d098f016421a6a340bf7ecbef
Angel-Karasu <angel.karasu@proton.me>
2024-04-16 13:35:56 +0200

    Update

--- app.py
+++ app.py
@@ -16,15 +16,18 @@ app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_methods=['*'], all
 def get_sortby_methods() -> list[str]: return [sortby.name for sortby in SortBy]
 
 @app.get('/get_supermarkets')
-def get_supermarkets(name:str = '', address:str = '', postal_code:int = '', city:str = '') -> list[tuple[int, SuperMarket]]:
-    sm = SuperMarket(name, Address(address, postal_code, city), '', {})
-    return [(id, supermarket) for id, supermarket in enumerate(SUPERMARKETS.supermarkets) if sm.include(supermarket)]
+def get_supermarkets() -> list[SuperMarket]: return SUPERMARKETS.supermarkets
 
 @app.get('/search_product')
 def search_product(
     search:str, page:int = 1, sortby:SortBy = SortBy.relevant, descending_order:bool = False,
     list_supermarket_id:list[int] = Query()
 ) -> list[tuple[int, Product]]: return SUPERMARKETS.search_products(list_supermarket_id, search, page, sortby, descending_order)
+
+@app.get('/search_supermarkets')
+def search_supermarkets(name:str = '', address:str = '', postal_code:int = '', city:str = '') -> list[tuple[int, SuperMarket]]:
+    sm = SuperMarket(name, Address(address, postal_code, city), '', {})
+    return [(id, supermarket) for id, supermarket in enumerate(SUPERMARKETS.supermarkets) if sm.include(supermarket)]
 
 @app.get('/supermarket/{id}/get_supermarket')
 def supermarket_get_supermarket(id:int = Path(ge=0, le=SUPERMARKETS.max_id)) -> SuperMarket: return SUPERMARKETS.supermarkets[id]
