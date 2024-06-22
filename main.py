e0854f1b850d0b93ab007518ddf2b46f37b057da
Angel-Karasu <angel.karasu@proton.me>
2024-06-22 22:12:56 +0200

    Add threading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/bin/python
 from heapq import merge
 from itertools import zip_longest
+from threading import Thread
 
 from utils.classes import Product, SortBy, SuperMarket
 from supermarkets import auchan, casino, intermarche, monoprix
@@ -10,10 +11,16 @@ class SuperMarkets:
     max_id:int = len(supermarkets) - 1
 
     def search_products(self, supermarkets_id: list[int], search:str, page:int, sortby:SortBy, descending_order:bool) -> list[tuple[int, Product]]:
-        list_products:list[list[tuple[int, Product]]] = [
-            [(id, product) for product in self.supermarkets[id].search_products(search, page, sortby, descending_order)]
-            for id in supermarkets_id
-        ]
+        list_products:list[list[tuple[int, Product]]] = []
+        threads:list[Thread] = []
+
+        for i, id in enumerate(supermarkets_id):
+            threads.append(Thread(
+                target=lambda id: list_products.append([(id, product) for product in self.supermarkets[id].search_products(search, page, sortby, descending_order)]), args=(id,)
+            ))
+            threads[i].start()
+            
+        for th in threads: th.join()
 
         return (
             [product for products in zip_longest(*list_products, fillvalue=None) for product in products if product is not None]
